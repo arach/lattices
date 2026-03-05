d8a8bc3514c147a76142afa442c4a89bd34a9bd4
Arach <arach@tchoupani.com>
2026-03-04 19:06:06 -0500

    🔧 Remove unused DownloadIcon component

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -75,16 +75,6 @@ function CheckIcon() {
   );
 }
 
-function DownloadIcon() {
-  return (
-    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} width="16" height="16">
-      <path d="M21 15v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4" />
-      <polyline points="7 10 12 15 17 10" />
-      <line x1="12" y1="15" x2="12" y2="3" />
-    </svg>
-  );
-}
-
 function AppleIcon() {
   return (
     <svg viewBox="0 0 384 512" fill="currentColor" width="14" height="14">
